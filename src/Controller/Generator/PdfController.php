31ee3a2f2cc990ab242b93d1c74c578598453cde
Dylan Groux <dylangroux2105@gmail.com>
2025-03-13 14:05:24 +0100

    Enhance PDF generation by adding file existence check and filling form fields

--- src/Controller/Generator/PdfController.php
+++ src/Controller/Generator/PdfController.php
@@ -23,13 +23,33 @@
             //On donne le chemin du PDF
             $pdfPath = $this->getParameter('kernel.project_dir') . '/public/pdf/2033-sd_5015form.pdf';
 
-            //On Récupère le pdf via son pdfPath et on défini son type et sa disposition
-            return new Response(file_get_contents($pdfPath), 200, [
+            // Vérifie si le fichier existe
+            if (!file_exists($pdfPath)) {
+                return new JsonResponse(['error' => 'Le fichier PDF est introuvable !'], 404);
+            }
+
+            //si l'id du formulaire correspond alors remplis sa valeur sinon null (par défault)
+            $fieldValues = [
+                'd#C3#A9signation_entreprise' => 'Salut tous le monde',
+            ];
+
+            //Définit les variables du PDF
+            $pdf = new Pdf($pdfPath);
+
+            //fillForm méthode qui permet de récupérer les champs form du PDF les analysers récupérer leurs id type etc et en paramètre peut permettre la modification de ces derniers.
+            $pdf->fillForm($fieldValues)
+                ->needAppearances()
+                ->saveAs($this->getParameter('kernel.project_dir') . '/public/pdf/2033-sd_5015form_filled.pdf');
+
+            //On Récupère le pdf via son pdfPath et on défini son type et sa disposition puis le génère
+            return new Response(file_get_contents($this->getParameter('kernel.project_dir') . '/public/pdf/2033-sd_5015form_filled.pdf'), 200, [
                 'Content-Type' => 'application/pdf',
                 'Content-Disposition' => 'inline; filename="2033-sd_5015form.pdf"',
             ]);
         }
 
+
+        //Route qui permet de vérifier la totalité des champs présent dans un PDF.
         #[Route('/pdf/fields', name: 'app_pdf_fields')]
         public function getPdfFields(): JsonResponse
         {
