25564ad6b6ff556644841da40b6184d4dda5bbf2
Dylan Groux <dylangroux2105@gmail.com>
2025-03-31 18:21:26 +0200

    readme + tableau associa

--- src/Controller/Generator/PdfController.php
+++ src/Controller/Generator/PdfController.php
@@ -7,13 +7,10 @@
     use Doctrine\ORM\EntityManagerInterface;
     use Symfony\Component\Routing\Annotation\Route;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use setasign\Fpdi\Fpdi;
-    use setasign\Fpdi\PdfReader;
     use Symfony\Component\HttpFoundation\JsonResponse;
     use mikehaertl\pdftk\Pdf;
 
     use App\Entity\User;
-
 
     class PdfController extends AbstractController
     {
@@ -30,7 +27,100 @@
 
             //si l'id du formulaire correspond alors remplis sa valeur sinon null (par défault)
             $fieldValues = [
-                'd#C3#A9signation_entreprise' => 'Salut tous le monde',
+                /* <----! DGFiP N°2033-A-SD 2025 !----> */
+                'designation_entreprise' => "",
+                'adresse_entreprise' => "",
+                'siret' => "",
+                'duree_exercice_mois' => '',
+                'duree_exercice' => '',
+                'duree_exercice_n_clos' => '',
+
+                /* <----! ACTIF !----> */
+                  /* <--! BRUT !--> */
+                '010_fondsCommercial_brut' => '',
+                '014_autres_brut' => '',
+                '028_immobiliCorporelles_brut' => '',
+                '040_immobiliFinanciere_brut' => '',
+                '044_total1_brut' => '',
+                '050_stock_matierePremiere_brut' => '',
+                '060_stock_marchandise_brut ' => '',
+                '064_avances_acompte_brut' => '',
+                '068_creances_clientsEtCompte_brut'=> '',
+                '072_creances_autres_brut' => '',
+                '080_valeursMobilieres_brut' => '',
+                '084_disponibilites_brut' => '',
+                '092_chargesConstatees_brut' => '',
+                '096_total2_brut' => '',
+                '110_totalGeneral_brut' => '',
+
+                  /* <--! AMORTISSEMENT !--> */
+                 '012_fondsCommercial_amortissements' => '',
+                 '016_autres_amortissements' => '',
+                 '030_immobiliCorporelles_amortissements' => '',
+                 '042_immobiliFinanciere_amortissements' => '',
+                 '048_total1_amortissements' => '',
+                 '052_stock_matierePremiere_amortissements' => '',
+                 '062_stock_marchandise_amortissements' => '',
+                 '066_avances_acompte_amortissements' => '',
+                 '070_creances_clientsEtCompte_amortissements' => '',
+                 '074_creances_autres_amortissements' => '',
+                 '082_valeursMobilieres_amortissements' => '',
+                 '086_disponibilites_amortissements' => '',
+                 '094_chargesConstatees_amortissements' => '',
+                 '098_total2_amortissements' => '',
+                 '112_totalGeneral_amortissements' => '',
+
+                  /* <--! NET!--> */
+                 '012_fondsCommercial_net' => '',
+                 '016_autres_net' => '',
+                 '030_immobiliCorporelles_net' => '',
+                 '042_immobiliFinanciere_net' => '',
+                 '048_total1_net' => '',
+                 '052_stock_matierePremiere_net' => '',
+                 '062_stock_marchandise_net' => '',
+                 '066_avances_acompte_net' => '',
+                 '070_creances_clientsEtCompte_net' => '',
+                 '074_creances_autres_net' => '',
+                 '082_valeursMobilieres_net' => '',
+                 '086_disponibilites_net' => '',
+                 '094_chargesConstatees_net' => '',
+                 '098_total2_net' => '',
+                 '112_totalGeneral_net' => '',
+
+                  /* <--! PASSIF !--> */
+                  /* <--! CAPITAUX PROPRES!--> */
+                 '120_capitalSocialOuIndividuel_capitauxP' => '',
+                 '124_ecartsReeval_capitauxP' => '',
+                 '126_reserveLegal_capitauxP' => '',
+                 '130_reserveReglementee_capitauxP' => '',
+                 '131_reserveOeuvreDArt_capitauxP' => '',
+                 '132_autresReserves_capitauxP' => '',
+                 '134_reportNouveau_capitauxP' => '',
+                 '136_resultatExercice_capitauxP' => '',
+                 '137_subventionInvestissement_capitauxP' => '',
+                 '140_provisionsReglementees_capitauxP' => '',
+                 '142_total1_capitauxP' => '',
+                 '142_total2_capitauxP' => '',
+
+                  /* <--! DETTES !--> */
+                 '156_empruntsEtDettes_dettes' => '',
+                 '164_avancesEtAcomptesRecus_dettes' => '',
+                 '166_fournisseursEtComptes_dettes' => '',
+                 '169_TVA_dettesFiscalesEtSociales_dettes' => '',
+                 '172_dettesFiscalesEtSociales_dettes' => '',
+                 '173_comptesCourantsAssocies_dettes' => '',
+                 '175_autresDettes_dettes' => '',
+                 '174_produitsConstates_dettes' => '',
+                 '176_total3_dettes' => '',
+                 '180_totalGeneral_dettes' => '',
+
+                  /* <--! RENVOIS !--> */
+                 '193_1_immobilisationFinancieresMoins1an_renvois' => '',
+                 '197_2_creancePlus1an_renvois' => '',
+                 '199_3_compteCourantAssociesDebiteurs_renvois' => '',
+                 '195_4_dettesPlus1an_renvois' => '',
+                 '182_5_coutRevientImmoAcquisePendantExercice_renvois' => '',
+                 '184_5_prixVenteHorsTVAdesImmoCedeesPendantExercice_renvois' => '',
             ];
 
             //Définit les variables du PDF
